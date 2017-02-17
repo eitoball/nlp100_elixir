9c2778527b3d8bf5efb3b7fed70c64f1b749176a
Eito Katagiri <eitoball@gmail.com>
2017-02-18 08:05:55 +0900

    solve 00. Reverse string

---
+++ ch1/n00.ex
@@ -0,0 +1,5 @@
+defmodule Nlp100.Ch1.N00 do
+  def call(str \\ "stressed") do
+    str |> String.reverse
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,10 @@
+defmodule Nlp100.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :nlp100,
+     version: "0.0.1",
+     elixir: "~> 1.4",
+     elixirc_paths: ["ch1"]]
+  end
+end
